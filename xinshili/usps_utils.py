28de6a581829dfe4ff2110a3e0409c6b8a188d7f
KunPengZheng <1258520250@qq.com>
2025-01-26 16:32:50 +0800

    .

--- xinshili/usps_utils.py
+++ xinshili/usps_utils.py
@@ -14,16 +14,6 @@ def track(lists):
     )
 
     data_str = resp.json()
+    print(data_str)
 
-    # 替换 JSON 不合法部分
-    data_str = data_str.replace("'", '"')  # 将单引号替换为双引号
-    data_str = data_str.replace("True", "true").replace("False", "false")  # 转换布尔值
-    data_str = data_str.replace("None", "null")  # 转换 None 为 null
-
-    # 尝试加载为 JSON
-    try:
-        data = json.loads(data_str)
-        print("修正后的 JSON 数据：", json.dumps(data, indent=4, ensure_ascii=False))
-        return data
-    except json.JSONDecodeError as e:
-        print("JSON 格式错误:", e)
+    return data_str
