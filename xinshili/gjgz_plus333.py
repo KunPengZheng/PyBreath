fdc02824a83822566bd0abffcac58fc4d9d0e898
KunPengZheng <1258520250@qq.com>
2025-05-13 09:40:25 +0800

    .

--- xinshili/gjgz_plus333.py
+++ xinshili/gjgz_plus333.py
@@ -1804,6 +1804,9 @@ def automatic(dir_path, analyse_obj):
                 go(analyse_obj, xlsx_path)
             else:
 
+                shipment_received_interval2_list = get_shipment_received_numbers(xlsx_path, gz_time)
+                change_shipment_received_count = len(shipment_received_interval2_list)
+
                 output_file = os.path.splitext(xlsx_path)[0] + "_去重.xlsx"
 
                 # 同一单会有多个sku，多个sku会生成多行数据，分析sku的时候不能去重，其它的需要去重
@@ -1861,10 +1864,11 @@ def automatic(dir_path, analyse_obj):
                 no_tracking_countl = round2((no_tracking_count_int / total_count_int) * 100)
                 tracking_countl = round2((tracking_count_int / total_count_int) * 100)
                 tracking_zero_countl = round2((tracking_zero_count_int / total_count_int) * 100)
+                change_shipment_received_countl = round2((change_shipment_received_count / total_count_int) * 100)
 
                 delete_file(output_file)
 
-                if (swl < 99):
+                if swl < 99 or change_shipment_received_count >= 10 or unpaid_count > 0:
                     go(analyse_obj, xlsx_path)
 
 
@@ -1872,10 +1876,6 @@ def call2():
     automatic("/Users/zkp/Desktop/B&Y/轨迹统计/zbw/2025.5/", ClientConstants.zbw)
     automatic("/Users/zkp/Desktop/B&Y/轨迹统计/sanrio/2025.5/", ClientConstants.sanrio)
     automatic("/Users/zkp/Desktop/B&Y/轨迹统计/xyl/2025.5/", ClientConstants.xyl)
-    # go(ClientConstants.zbw, "/Users/zkp/Desktop/B&Y/轨迹统计/zbw/2025.5/创建时间7_520.xlsx")
-    # go(ClientConstants.xyl, "/Users/zkp/Desktop/B&Y/轨迹统计/xyl/2025.5/创建时间10_161.xlsx")
-
-
 
 
 if __name__ == '__main__':
