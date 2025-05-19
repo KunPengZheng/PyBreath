7a432cbf4fc03c0145356289e76de72fec834a3c
KunPengZheng <1258520250@qq.com>
2025-05-19 14:36:40 +0800

    .

--- xinshili/dianxiaomi_yifu.py
+++ xinshili/dianxiaomi_yifu.py
@@ -1,44 +1,42 @@
-import pandas as pd
 import numpy as np
-
 import pandas as pd
 import re
+from dataclasses import dataclass
+from xinshili import utils, openpyxl_utils
 
 
-def normalize_punctuation_spacing(file_path, output_path, sex):
-    # 读取 Excel 文件
-    df = pd.read_excel(file_path, dtype=str)
+@dataclass(frozen=True)
+class Combined:
+    White = '白'
+    Black = '黑'
+    Man = '男'
+    Women = '女'
+    Nones = '未知'
 
+
+def normalize_punctuation_spacing(content, category_keyword):
     # 定义要处理的标点符号（常见英文标点）
     punctuations = [",", "\\.", "!", "\\?", ";", ":"]
 
     # 构造正则表达式：匹配标点符号后无空格 或 多空格 的情况
     pattern = re.compile(r"({})(?=\S)|({})\s{{2,}}".format("|".join(punctuations), "|".join(punctuations)))
 
-    def fix_spacing(text):
+    def fix_spacing(text, ):
         if not isinstance(text, str):
-            return text
+            return text + f"{category_keyword}"
+
         # 标准化空格（标点后添加一个空格）
         text = pattern.sub(lambda m: m.group(0)[0] + " ", text)
 
         # 删除多余的空格（多个空格 → 一个空格）
         text = re.sub(r'\s{2,}', ' ', text)
 
-        text = text + f", {sex}"
+        text = text + f"{category_keyword}"
 
         # 去除开头/结尾空格
         return text.strip()
 
-    # 只处理 C列
-    if df.shape[1] >= 3:  # 确保有至少3列（C列）
-        df.iloc[:, 2] = df.iloc[:, 2].apply(fix_spacing)
-    else:
-        print("❌ 文件列数不足，无法找到C列。")
-        return
-
-    # 保存到输出文件
-    df.to_excel(output_path, index=False)
-    print(f"✅ 标点处理完成，结果已保存至: {output_path}")
+    return fix_spacing(content)
 
 
 def copy(
@@ -46,8 +44,9 @@ def copy(
         file2_path: object,
         output_path: object,
         lunbotu: object,
-        color_value: object = "Black",
-        stock_quantity: object = 60
+        category_keyword,
+        color_value: object,
+        stock_quantity: object = 77.1
 ) -> object:
     df1 = pd.read_excel(file1_path, header=None, dtype=str)
     df2 = pd.read_excel(file2_path, dtype=str)
@@ -99,6 +98,8 @@ def copy(
             row_idx = len(df2)
             df2.loc[row_idx] = [np.nan] * len(df2.columns)
 
+        normalize_punctuation_spacing(val_a, category_keyword)
+
         df2.at[row_idx, col_A] = val_a  # file1 的 C → A
         df2.at[row_idx, col_B] = val_a  # file1 的 C → B
         df2.at[row_idx, col_C] = val_b  # file1 的 B → C
@@ -123,17 +124,51 @@ def copy(
     print(f"✅ 数据已处理并保存到: {output_path}")
 
 
-if __name__ == '__main__':
-    # 示例用法（替换路径）
-    file1 = "/Users/zkp/Desktop/B&Y/轨迹统计/flld/2025.5/副本白色女装Q401-Q550(1).xlsx"
-    file2 = "/Users/zkp/Documents/import_created_product_popTemu_副本.xlsx"
-    output = "/Users/zkp/Documents/result6666.xlsx"
+def detect_keywords(content):
+    # 判断颜色
+    if Combined.White in content:
+        color_flag = Combined.White
+    elif Combined.Black in content:
+        color_flag = Combined.Black
+    else:
+        color_flag = Combined.Nones
 
-    sex_man = "Man"
+    # 判断性别
+    if Combined.Man in content:
+        gender_flag = Combined.Man
+    elif Combined.Women in content:
+        gender_flag = Combined.Women
+    else:
+        gender_flag = Combined.Nones
 
-    sex_women = "Women"
+    return color_flag, gender_flag
 
-    normalize_punctuation_spacing(file1, file1, sex_women)
+
+def handler(src_path):
+    file_name_with_extension = utils.get_filename_with_extension(src_path)
+    color_flag, gender_flag = detect_keywords(file_name_with_extension)
+    if color_flag == Combined.Nones or gender_flag == Combined.Nones:
+        raise ValueError(f"文件名不存在 '男女'或'黑白' 等关键字！！！")
+
+    template_path = openpyxl_utils.load_excel_file(
+        utils.current_dir() + "/xlsx/dxm/import_created_product_popTemu.xlsx")
+    output = f"/Users/zkp/Desktop/B&Y/dxm/{file_name_with_extension}"
+
+    sex_man = ". 2025 Men's T-shirt"
+    sex_women = ". 2025 Women's T-shirt"
+    result_sex = ""
+    if color_flag == Combined.Man:
+        result_sex = sex_man
+    else:
+        result_sex = sex_women
+
+    white = "White"
+    black = "Black"
+    result_color = ""
+    if color_flag == Combined.White:
+        result_color = white
+    else:
+        result_color = black
 
     lunbotu_white = "\nhttps://wxalbum-10001658.image.myqcloud.com/wxalbum/1573179/20250515101012/616145e8af459c7495758a9aec2e5f37.jpg" \
                     "\nhttps://wxalbum-10001658.image.myqcloud.com/wxalbum/1573179/20250515101012/db23824b806321f7b39f43137994f780.jpg" \
@@ -141,20 +176,29 @@ if __name__ == '__main__':
                     "\nhttps://wxalbum-10001658.image.myqcloud.com/wxalbum/1573179/20250515101014/2a1066b0bf75ae5c0ae23e580448c986.jpg" \
                     "\nhttps://wxalbum-10001658.image.myqcloud.com/wxalbum/1573179/20250515101015/ce1447f258253c875aa2b0b4883b59b1.jpg"
 
-    # lunbotu_black = "\nhttps://wxalbum-10001658.image.myqcloud.com/wxalbum/1573179/20250515101016/98e779932fa49e38c4b95fb57b13393a.jpg" \
-    #                 "\nhttps://wxalbum-10001658.image.myqcloud.com/wxalbum/1573179/20250515101016/957edc530ac9905385533f9c0f4eb7bc.jpg" \
-    #                 "\nhttps://wxalbum-10001658.image.myqcloud.com/wxalbum/1573179/20250515101017/bff7b4be4481c5c6e11d2edf1a8d9fc6.jpg" \
-    #                 "\nhttps://wxalbum-10001658.image.myqcloud.com/wxalbum/1573179/20250515101017/17666fe45e9b5f1254135febbd624721.jpg" \
-    #                 "\nhttps://wxalbum-10001658.image.myqcloud.com/wxalbum/1573179/20250515101018/d4acb5ca0dce2d7be4fc88a00ce00b44.jpg"
-
-    white = "White"
-    black = "Black"
+    lunbotu_black = "\nhttps://wxalbum-10001658.image.myqcloud.com/wxalbum/1573179/20250515101016/98e779932fa49e38c4b95fb57b13393a.jpg" \
+                    "\nhttps://wxalbum-10001658.image.myqcloud.com/wxalbum/1573179/20250515101016/957edc530ac9905385533f9c0f4eb7bc.jpg" \
+                    "\nhttps://wxalbum-10001658.image.myqcloud.com/wxalbum/1573179/20250515101017/bff7b4be4481c5c6e11d2edf1a8d9fc6.jpg" \
+                    "\nhttps://wxalbum-10001658.image.myqcloud.com/wxalbum/1573179/20250515101017/17666fe45e9b5f1254135febbd624721.jpg" \
+                    "\nhttps://wxalbum-10001658.image.myqcloud.com/wxalbum/1573179/20250515101018/d4acb5ca0dce2d7be4fc88a00ce00b44.jpg"
+    result_lbt = ""
+    if color_flag == Combined.White:
+        result_lbt = lunbotu_white
+    else:
+        result_lbt = lunbotu_black
 
     copy(
-        file1_path=file1,
-        file2_path=file2,
+        file1_path=src_path,
+        file2_path=template_path,
         output_path=output,
-        lunbotu=lunbotu_white,
-        color_value=white,  # 可变参数
-        stock_quantity=77.1  # 可变参数
+        lunbotu=result_lbt,
+        category_keyword=result_sex,
+        color_value=result_color
     )
+
+    utils.open_dir(output)
+
+
+if __name__ == '__main__':
+    src = "/Users/zkp/Library/Containers/com.tencent.xinWeChat/Data/Library/Application Support/com.tencent.xinWeChat/2.0b4.0.9/aee968804ccf60699f2aada7c6e578a8/Message/MessageTemp/24fe1b1c873f588c7b3f70c4efe61bb7/File/白色女装Y401-500上传表格.xlsx"
+    handler(src)
