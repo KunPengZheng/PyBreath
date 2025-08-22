4c839b2fc0710bb5eaf330db62a4960b1e0eb169
KunPengZheng <1258520250@qq.com>
2025-08-22 15:17:27 +0800

    .

--- xinshili/dxm_xyl_yd_tack.py
+++ xinshili/dxm_xyl_yd_tack.py
@@ -82,14 +82,16 @@ def process_tracking_time1(file_path):
             outbound_time_flag = ship_time_str and ship_time_str.lower() != "nan"
 
             diff = None
-            if date_flag:
-                if time_flag:
+            if date_flag:  # 存在最新日期
+                if time_flag:  # 存在最新时间
                     latest_date_time = datetime.strptime(f"{date_str} {time_str}", "%Y-%m-%d %H:%M")
                 else:
                     latest_date_time = datetime.strptime(f"{date_str}", "%Y-%m-%d")
+                # 现在时间 - 最新日期时间
                 diff = utc0_now - latest_date_time
             elif outbound_time_flag:
                 creation_time = datetime.strptime(ship_time_str, "%Y-%m-%d %H:%M:%S")
+                # 现在时间 - 发货时间
                 diff = utc0_now - convert_china_to_utc0(creation_time)
 
             if diff is not None:
