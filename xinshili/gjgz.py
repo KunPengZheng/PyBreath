ba374830d45a2fb59932e9fdcbcb086597837c24
KunPengZheng <1258520250@qq.com>
2025-08-29 09:46:31 +0800

    .

--- xinshili/gjgz.py
+++ xinshili/gjgz.py
@@ -1,6 +1,8 @@
 from xinshili.flld_gjgz import call
 from xinshili.gjgz_plus333 import call2
+from xinshili.tk_dxm_gjgz import call3
 
 if __name__ == '__main__':
     call()
     call2()
+    call3()
