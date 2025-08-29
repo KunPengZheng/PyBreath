9e97ba7e61b31c4b8909bfdff89450ec3f7eb504
KunPengZheng <1258520250@qq.com>
2025-08-29 11:29:08 +0800

    .

--- xinshili/yd_gjgz_test.py
+++ xinshili/yd_gjgz_test.py
@@ -1,7 +1,7 @@
 import re
 
 from xinshili.yd_gjgz import get_specified_node_info, extract_first_date_time_node, extract_first_datetime_site, \
-       extract_info
+    extract_info, split_excel_by_date_and_unique_count
 
 arr = ['Delivered', 'Out for Delivery', 'Preparing for Delivery',
        'Moving Through Network\n\nIn Transit to Next Facility, Arriving On Time\n\nAugust 20, 2025',
@@ -21,30 +21,36 @@ arr = ['Delivered\n\nDelivered, Individual Picked Up at Post Office\n\nLAKESIDE,
        '\n\t\t\t\t\n\t\t\t\t\n\t\t\t\t\n\t\t\t\t\n\t\t\t\t\tOut for Delivery\n\t\t\t\t\t\n\t\t\t\t\t\t\t\t\n\t\t\t\t\t\t\t\t\n\t\t\t\t\t\t\t\t\n\t\t\t\t\t\t\t\t\tLAKESIDE, CA 92040 \n\t\t\t\t\t\t\t\t\t\n\t\t\t\t\t\t\t\t\n\t\t\t\t\t\t\t\t\n\t\t\t\t\t\t\t\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\tAugust 25, 2025,\n\t\t\t\t\t\t\t\t\t7:44 am\n\t\t\t\t\t\t\t\t\t\n\t\t\t\t\t\t\t\t\t\n\t\t\t\t\t',
        '\n\t\t\t\t\n\t\t\t\t\n\t\t\t\t\n\t\t\t\t\n\t\t\t\t\tArrived at Post Office\n\t\t\t\t\t\n\t\t\t\t\t\t\t\t\n\t\t\t\t\t\t\t\t\n\t\t\t\t\t\t\t\t\n\t\t\t\t\t\t\t\t\tLAKESIDE, CA 92040 \n\t\t\t\t\t\t\t\t\t\n\t\t\t\t\t\t\t\t\n\t\t\t\t\t\t\t\t\n\t\t\t\t\t\t\t\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\tAugust 25, 2025,\n\t\t\t\t\t\t\t\t\t7:33 am\n\t\t\t\t\t\t\t\t\t\n\t\t\t\t\t\t\t\t\t\n\t\t\t\t\t', ]
 
-objss = get_specified_node_info(arr)
+# objss = get_specified_node_info(arr)
 
-possession_first_event = objss["possession_first_event"]
-possession_second_event = objss["possession_second_event"]
-possession_last_event = objss["possession_last_event"]
-possession_newest_time_event = objss["possession_newest_time_event"]
+# possession_first_event = objss["possession_first_event"]
+# possession_second_event = objss["possession_second_event"]
+# possession_last_event = objss["possession_last_event"]
+# possession_newest_time_event = objss["possession_newest_time_event"]
+#
+# newest_dates, newest_times = extract_first_date_time_node(possession_newest_time_event)
+# newest_site = extract_first_datetime_site(possession_newest_time_event)
+#
+# print(f"possession_first_event:\n {possession_first_event}")
+# print(f"possession_second_event:\n {possession_second_event}")
+# print(f"possession_last_event:\n {possession_last_event}")
+# print(f"possession_newest_time_event:\n {possession_newest_time_event}")
+# print(f"newest_dates:\n {newest_dates}")
+# print(f"newest_times:\n {newest_times}")
+# print(f"newest_site:\n {newest_site}")
+#
+# result_map = {}
+# excel_result_map = {}
+# result_map["sss"] = {
+#                                         "PossessionFirstEvent": possession_first_event,
+#                                         "PossessionSecondEvent": possession_second_event,
+#                                         "PossessionLastEvent": possession_last_event,
+#                                         "PossessionNewestTimeEvent": possession_newest_time_event}
+#
+# extract_info(result_map,excel_result_map)
 
-newest_dates, newest_times = extract_first_date_time_node(possession_newest_time_event)
-newest_site = extract_first_datetime_site(possession_newest_time_event)
-
-print(f"possession_first_event:\n {possession_first_event}")
-print(f"possession_second_event:\n {possession_second_event}")
-print(f"possession_last_event:\n {possession_last_event}")
-print(f"possession_newest_time_event:\n {possession_newest_time_event}")
-print(f"newest_dates:\n {newest_dates}")
-print(f"newest_times:\n {newest_times}")
-print(f"newest_site:\n {newest_site}")
-
-result_map = {}
-excel_result_map = {}
-result_map["sss"] = {
-                                        "PossessionFirstEvent": possession_first_event,
-                                        "PossessionSecondEvent": possession_second_event,
-                                        "PossessionLastEvent": possession_last_event,
-                                        "PossessionNewestTimeEvent": possession_newest_time_event}
-
-extract_info(result_map,excel_result_map)
+# split_excel_by_date_and_unique_count(
+#     "/Users/zkp/Desktop/B&Y/轨迹统计/dxm_xyl_track/order_120250829111401423_1573179.xlsx",
+#     "发货时间", "订单号",
+#     "订单号", "发货时间",
+#     "/Users/zkp/Desktop/B&Y/轨迹统计/dxm_xyl_track")
