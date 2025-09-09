50183f3d752e14981d3c960835ec565e923f009d
KunPengZheng <1258520250@qq.com>
2025-09-09 15:52:01 +0800

    .

--- xinshili/yd_gjgz.py
+++ xinshili/yd_gjgz.py
@@ -636,12 +636,14 @@ def extract_and_process_data(filepath: str, column_name: str, wl_name=RowName.Tr
             lambda x: str(x).strip().lower() in [''])]
     else:
         filtered_data = data[data[column_name].apply(
-            lambda x: str(x).strip().lower() in ['', CourierStateMapValue.not_yet,
+            lambda x: str(x).strip().lower() in ['',
+                                                 CourierStateMapValue.not_yet,
                                                  CourierStateMapValue.pre_ship,
                                                  CourierStateMapValue.tracking,
-                                                 CourierStateMapValue.no_tracking,
-                                                 CourierStateMapValue.alert
-                                                 ])]
+                                                 CourierStateMapValue.no_tracking
+                                                 ]
+                      or str(x).strip().lower().startswith(CourierStateMapValue.alert)
+        )]
     # 提取符合条件的 'Tracking No./物流跟踪号' 列数据，并剔除wl_name列中 NaN，去除字符串前后空格，排除只含空格或本身为空的字符串
     items = [x.strip() for x in filtered_data[wl_name].dropna().astype(str) if x.strip() != '']
 
